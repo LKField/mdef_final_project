1b888dd542b3f46630c67c5d0f375a8328300c02
lucretiafield <lucretia.k.field@gmail.com>
2025-05-04 23:26:07 +0200

    adding doc strings

--- trackpadHandling.py
+++ trackpadHandling.py
@@ -24,8 +24,8 @@ class Story:
     
     """
     
-    entries = []
-    voltages = []
+    entries_lst = []
+    voltages_lst = []
     
     def __init__(self, name, led_pin, adc_pin, en_path, non_path, voltage=0):
         """Constructor method
@@ -47,8 +47,8 @@ class Story:
         self.led = Pin(led_pin, Pin.OUT)	# initialize the LED Pin
         self.sensor = ADC(adc_pin)			# initialize the ADC Pin
 
-        self.entries.append(self)			# append the new object to the entries list
-        self.voltages.append(voltage)		# append the voltage to the voltages list 
+        self.entries_lst.append(self)			# append the new object to the entries list
+        self.voltages_lst.append(voltage)		# append the voltage to the voltages list 
     
     def __str__(self):
         return f"{self.name}, Index: {self.index}, LED: {self.led_pin}, ADC: {self.adc_pin}, {self.en_path}, {self.non_path}, Voltage: {self.voltage}"
@@ -56,21 +56,28 @@ class Story:
         
     @classmethod   
     def updateVoltage(cls, index, voltage):
-        cls.voltages[index] = voltage
-        return cls.voltages
+        """class method to update the voltages in the voltages_lst list
+
+        :param index: index of in the voltages_lst to update
+        :type index: int
+        :param voltage: the value to update into the voltages_lst
+        :type voltage: int
+        
+        :return: voltages_lst (list of voltages)
+        :rtype: list of ints 
+        """
+        cls.voltages_lst[index] = voltage
+        return cls.voltages_lst
        
     @classmethod   
     def findMax(cls):
         """Find the maximum value from the passed list and returns the index and value
-
-        :param voltages: list of voltages to compare for maximum value
-        :type voltages: list of integers
         
         :return: ind (index of maximum), m (value of maximum)
         :rtype: ind (int), m (int) 
         """
-        m = max(cls.voltages)
-        ind = cls.voltages.index(m)
+        m = max(cls.voltages_lst)
+        ind = cls.voltages_lst.index(m)
         print(f"Index of Max: {ind}, Value of Max: {m}")
         return ind, m
 
@@ -95,26 +102,22 @@ if __name__ == "__main__":
     story1 = Story("Vitti", 25, 28, "audio/vitti/en.wav", "audio/vitti/non.wav", 0)
     story2 = Story("Denisa", 25, 27, "audio/denisa/en.wav", "audio/denisa/non.wav", 0)
     
-    print(Story.entries)
-    print(Story.voltages)
-    
-    #story1.setup()
+#    print(Story.entries_lst)
+#    print(Story.voltages_lst)
     
     while True: 
         # Read the ADC and update voltages (Will need to be replaced with SPI code for 8 channel ADC) 
-        for i in range(len(Story.entries)):
-            voltages = Story.updateVoltage(i, Story.entries[i].sensor.read_u16())
+        for i in range(len(Story.entries_lst)):
+            voltages = Story.updateVoltage(i, Story.entries_lst[i].sensor.read_u16())
             
-        print(Story.voltages)
-
         # Find the maximum ADC value 
         ind, max_value = Story.findMax()
 
         
         # Turn on and off the LED of the maximum - for now set to the onboard LED 
-        Story.entries[ind].ledOn()
+        Story.entries_lst[ind].ledOn()
         sleep(2)
-        Story.entries[ind].ledOff()
+        Story.entries_lst[ind].ledOff()
         sleep(2)
         
         
